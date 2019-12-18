07615b77e015b67ab4ae123965141a3c9f9a205a
Alon Ben Harosh <alonbh5@gmail.com>
2019-12-18 14:54:28 +0200

    kaka

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -7,7 +7,7 @@
 //after that enter your higth(3-20) and width(3-80) of the maze: an un-even number
 //if your choose 1: enter the string of chars with only: '*' for wall, or 'space' for blank
 //the perimeter most be only * and an opening point at 1,0 and h-2,w-1
-//enjoy!!
+//enjoy!
 int main()
 {
 
