739a7d4dc6ce6978c8e333206d1cb1d00718e87a
Alon Ben Harosh <alonbh5@gmail.com>
2019-12-18 14:41:30 +0200

    old :(

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -1,9 +1,8 @@
 #include "Maze.h"
 
-
-//Maze Creator and Solver 
-//by Alon Ben Harosh 
-//Shira ostrinsky 311119002 
+//Maze Creator and Solver
+//by Alon Ben Harosh
+//Shira ostrinsky 311119002
 //Choose 1 to enter your own maze by and 2 for the Random Creation of a maze
 //after that enter your higth(3-20) and width(3-80) of the maze: an un-even number
 //if your choose 1: enter the string of chars with only: '*' for wall, or 'space' for blank
@@ -11,28 +10,30 @@
 //enjoy!!
 int main()
 {
-	
+
 	int choice, h, w;
+
+
 	cout << "Maze: 1) From input 2) Random" << endl;
+
 	cin >> choice;
+
 	cin >> h;
+
 	cin >> w;
-	cout<< endl;
-	
-	Maze maze(h, w);
+	cout << endl;
+
+	Maze shira(h, w);
 
 
 	if (choice == 1)
 	{
-		if (!(maze.buildMaze()))
-		{
+		if (shira.buildMaze() == false)
 			cout << "invalid input" << endl;
-			exit(1);
-		}
 		else
 		{
-			if (maze.solve())
-				maze.print();
+			if (shira.solve())
+				shira.print();
 			else
 				cout << "no solution";
 		}
@@ -42,11 +43,11 @@ int main()
 			exit(5);
 		else
 		{
-			maze.create();
-			if (maze.solve())
-				maze.print();
+			shira.create();
+			if (shira.solve())
+				shira.print();
 			else
 				cout << "no solution"; //:)
 		}
-	
+	   	 
 }
